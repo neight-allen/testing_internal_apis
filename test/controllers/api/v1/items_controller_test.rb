e5410d0ba7a4623281703390fa500485d9e34af1
Nate Allen <neight.allen@gmail.com>
2016-04-12 10:10:27 -0600

    Added create test

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -14,12 +14,32 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
 
   test "#show" do
     id = Item.first.id
+    name = "MyString"
 
     get :show, id: id, format: :json
     item = JSON.parse(response.body)
 
     assert_response :success
     assert_equal item["id"], id
+    assert_equal item["name"], name
+  end
+
+  test "#create" do
+    item_params = {name: "Hammer", description: "When it is this time, you stop"}
+
+    assert_equal 2, Item.count
+
+    post :create, format: :json, item: item_params
+
+    item_count_after = Item.count
+
+    newly_created_item = JSON.parse(response.body)
+
+    assert_response :success
+    assert_equal 3, item_count_after
+    assert_equal item_params[:name], Item.last.name
+    assert_equal Item.last.id, newly_created_item["id"]
+
   end
 
 end
